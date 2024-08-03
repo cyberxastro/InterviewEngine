f1e501b09a4038e02741f17676d4c8454a64a1f0
Gaurav Sharad Suryawanshi <gauravss3703@gmail.com>
2024-08-04 00:35:13 +0530

    main

--- drizzle.config.js
+++
@@ -1,9 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./utils/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://neondb_owner:EPF1rIZe4GTp@ep-flat-band-a18v9utc.ap-southeast-1.aws.neon.tech/InterviewEngine?sslmode=require',
-    }
-  };
-  
